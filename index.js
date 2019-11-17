079dbbe659e5e1f326f1932566b115ece8223a02
Wendy Maxwell <50604460+WendyMaxwell@users.noreply.github.com>
2019-11-17 05:25:39 -0700

    added logic for postal zones

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
-const cool = require('cool-ascii-faces')
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
-
-/*const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true
-});*/
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
@@ -16,35 +9,18 @@ express()
 
   .get('/', (req, res) => res.render('pages/index'))
 
-  //.get('/getRate', (req, res) => res.render('pages/result'))
   .get('/getRate', calculateRate)
 
-  /*.get('/db', async (req, res) => {
-    try {
-      const client = await pool.connect()
-      const result = await client.query('SELECT * FROM test_table');
-      const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
-      client.release();
-    } catch (err) {
-      console.error(err);
-      res.send("Error " + err);
-    }
-  })*/
-  //.get('/cool', (req, res) => res.send(cool()))
-  //.get('/times', (req, res) => res.send(showTimes()))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-
 
   function calculateRate(req, res) {
   	const weight = Number(req.query.weight)
   	const mail_rate = req.query.mail_rate
+  	const zone = Number(req.query.zone)
 
-  	//console.log(weight)
-  	//console.log(mail_rate)
-
-  	var result = "0"
+  	var result = ""
   	var mail_method = "Error"
+  	var zone_number = ""
 
   	switch (mail_rate) {
   		case "stamped":
@@ -133,60 +109,174 @@ express()
 
 	  	case "package":
 	  		mail_method = "First-Class Package Service—Retail"
-	  		if (weight > 0 && weight <= 1) {
-	  			result = "$ 3.66"
-	  		}
-	  		else if (weight > 1 && weight <= 2) {
-	  			result = "$ 3.66"
-	  		}
-	  		else if (weight > 2 && weight <= 3) {
-	  			result = "$ 3.66"
-	  		}
-	  		else if (weight > 3 && weight <= 4) {
-	  			result = "$ 3.66"
-	  		}
-	  		else if (weight > 4 && weight <= 5) {
-	  			result = "$ 4.39"
-	  		}
-	  		else if (weight > 5 && weight <= 6) {
-	  			result = "$ 4.39"
-	  		}
-	  		else if (weight > 6 && weight <= 7) {
-	  			result = "$ 4.39"
-	  		}
-	  		else if (weight > 7 && weight <= 8) {
-	  			result = "$ 4.39"
-	  		}
-	  		else if (weight > 8 && weight <= 9) {
-	  			result = "$ 5.19"
-	  		}
-	  		else if (weight > 9 && weight <= 10) {
-	  			result = "$ 5.19"
-	  		}
-	  		else if (weight > 10 && weight <= 11) {
-	  			result = "$ 5.19"
-	  		}
-	  		else if (weight > 11 && weight <= 12) {
-	  			result = "$ 5.19"
-	  		}
-	  		else if (weight > 12 && weight <= 13) {
-	  			result = "$ 5.71"
-	  		}
-	  		else {
-	  			result = "not available (invalid weight)"
+	  		switch (zone) {
+	  			case 1:
+	  				zone_number = "to zone 1"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.66"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.39"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.19"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.71"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 2:
+			  		zone_number = "to zone 2"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.66"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.39"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.19"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.71"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 3:
+			  		zone_number = "to zone 3"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.70"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.44"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.24"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.78"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 4:
+			  		zone_number = "to zone 4"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.74"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.49"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.30"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.85"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 5:
+			  		zone_number = "to zone 5"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.78"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.53"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.35"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.93"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 6:
+			  		zone_number = "to zone 6"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.82"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.57"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.40"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 5.99"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 7:
+			  		zone_number = "to zone 7"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 3.94"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.69"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.53"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 6.13"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 8:
+			  		zone_number = "to zone 8"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 4.06"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.81"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.66"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 6.27"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
+			  	case 9:
+			  		zone_number = "to zone 9"
+		  			if (weight > 0 && weight <= 4) {
+		  			result = "$ 4.06"
+			  		}
+			  		else if (weight > 4 && weight <= 8) {
+			  			result = "$ 4.81"
+			  		}
+			  		else if (weight > 8 && weight <= 12) {
+			  			result = "$ 5.66"
+			  		}
+			  		else if (weight > 12 && weight <= 13) {
+			  			result = "$ 6.27"
+			  		}
+			  		else {
+			  			result = "not available (invalid weight)"
+			  		}
+			  		break;
 	  		}
 	  		break;
   	}
 
-  	const params = {weight: weight, mail_method: mail_method, result: result}
+  	const params = {weight: weight, mail_method: mail_method, zone_number: zone_number, result: result}
 
   	res.render('pages/result', params)
   }
-  /*showTimes = () => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-      result += i + ' '
-    }
-    return result;
-  }*/
